239d35bf2287f3757db3ab4e5cc7c3cc5195faa1
wowowork12345 <wowowork12345>
2023-12-18 14:49:38 +0800

    調整個顏色和格子距離

--- src/App.vue
+++ src/App.vue
@@ -6,38 +6,69 @@ const path = useRoute().path;
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-sm navbar-dark bg-warning">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-success">
     <div class="container-fluid">
       <a class="navbar-brand" href="/emp/index">
         <!-- <router-link to="/emp/index"> Taiwan High SpeedRail</router-link> -->
         Taiwan High SpeedRail
       </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#mynavbar"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="mynavbar">
         <ul class="navbar-nav me-auto">
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="shoppingDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">班次管理系統</a>
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="shoppingDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+              >班次管理系統</a
+            >
             <ul class="dropdown-menu" aria-labelledby="shoppingDropdown">
-              <li >
-                <button class="dropdown-item"> 
-                  <router-link to="/schedule/train"  id="fontcolor">高鐵列車</router-link>
+              <li>
+                <button class="dropdown-item">
+                  <router-link to="/schedule/train" id="fontcolor"
+                    >高鐵列車</router-link
+                  >
                 </button>
               </li>
               <li>
                 <button class="dropdown-item">
-                  <router-link to="/schedule/ticketDiscount" class="nav-link" id="fontcolor">車票優惠</router-link>
+                  <router-link
+                    to="/schedule/ticketDiscount"
+                    class="nav-link"
+                    id="fontcolor"
+                    >車票優惠</router-link
+                  >
                 </button>
               </li>
               <li>
                 <button class="dropdown-item">
-                  <router-link to="/schedule/railRoute" class="nav-link" id="fontcolor">高鐵路線</router-link>
+                  <router-link
+                    to="/schedule/railRoute"
+                    class="nav-link"
+                    id="fontcolor"
+                    >高鐵路線</router-link
+                  >
                 </button>
               </li>
               <li>
                 <button class="dropdown-item">
-                  <router-link to="/schedule/scheduleTemplate" class="nav-link" id="fontcolor">班次模板</router-link >
+                  <router-link
+                    to="/schedule/scheduleTemplate"
+                    class="nav-link"
+                    id="fontcolor"
+                    >班次模板</router-link
+                  >
                 </button>
               </li>
             </ul>
@@ -50,3 +81,15 @@ const path = useRoute().path;
     <router-view></router-view>
   </section>
 </template>
+<style>
+#app {
+  padding: 0;
+  margin: 0;
+}
+div {
+  margin: 0;
+}
+.container {
+  margin: 20px;
+}
+</style>
